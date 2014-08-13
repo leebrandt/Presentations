81e281a747668d1fe458ae98bbef4ebe010f2573
Lee Brandt <leebrandt@gmail.com>
2014-08-13 00:37:13 -0500

    Added iOS Talk

--- JavaScriptLevelUp/General/ScopeAndClosures.js
+++ JavaScriptLevelUp/General/ScopeAndClosures.js
@@ -1,22 +1,29 @@
-var newClosure = closure();
+function nonClosure() {
+  'use strict';
 
-console.log(newClosure());
-setTimeout(
-	function(){
-		console.log(newClosure())
-	}, 500);
-
-
-function nonClosure(){
-	var date = new Date();
+  var date = new Date();
 	return date.getMilliseconds();
 }
 
 // ########## Closure ############## //
-function closure(){
-	var date = new Date();
-	return function(){
-		return date.getMilliseconds();
-	}
+function closure() {
+  'use strict';
+
+  var date = new Date();
+  return function(){
+    return date.getMilliseconds();
+  }
 }
 
+(function(){
+  'use strict';
+
+  var newClosure = closure();
+
+  console.log(newClosure());
+  setTimeout(
+  	function(){
+  		console.log(newClosure())
+  	},
+    500);
+})()
--- JavaScriptLevelUp/General/UseStrictPurpose.js
+++ JavaScriptLevelUp/General/UseStrictPurpose.js
@@ -1,5 +1,4 @@
 (function(){
-	
 	'use strict';
 
 	value = 'new value';
